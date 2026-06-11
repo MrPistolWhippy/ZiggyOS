0fa977bddf700e651c198a6acdeb5855a25cf502
MrPistolWhippy <mrpistolwhippy@://github.com>
2026-06-11 03:18:09 +0000

    Feature: Establish x86 Interrupt Descriptor Table vector structure

--- src/mmu.c
+++ src/mmu.c
@@ -56,3 +56,42 @@ void free(void* ptr, unsigned int size) {
 		heap_current = addr;
 	}
 }
+
+struct idt_entry {
+	unsigned short base_lo;
+	unsigned short sel;
+	unsigned char always0;
+	unsigned char flags;
+	unsigned short base_hi;
+} __attribute__((packed));
+struct idt_ptr {
+	unsigned short limit;
+	unsigned int base;
+} __attribute__((packed));
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags) {
+	idt[num].base_lo = (base \& 0xFFFF);
+	idt[num].base_hi = (base >> 16) \& 0xFFFF;
+	idt[num].sel = sel;
+	idt[num].always0 = 0;
+	idt[num].flags = flags;
+}
+struct idt_ptr {
+	unsigned short limit;
+	unsigned int base;
+} __attribute__((packed));
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags) {
+	idt[num].base_lo = (base \& 0xFFFF);
+	idt[num].base_hi = (base >> 16) \& 0xFFFF;
+	idt[num].sel = sel;
+	idt[num].always0 = 0;
+	idt[num].flags = flags;
+}
+void init_idt() {
+	idtp.limit = (sizeof(struct idt_entry) * 256) - 1;
+	idtp.base = (unsigned int)\&idt;
+	for(int i = 0; i < 256; i++) idt_set_gate(i, 0, 0, 0);
+	__asm__ __volatile__("lidt 
