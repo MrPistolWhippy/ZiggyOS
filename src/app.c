453dc5aaabaed2c5cc34a2b0fea3aac8d39b85a8
MrPistolWhippy <mrpistolwhippy@://github.com>
2026-06-11 04:57:24 +0000

    Pipeline: Force trigger fresh cloud compiler run for build execution loop

--- src/app.c
+++ src/app.c
@@ -1,6 +1,5 @@
 #include "io.h"
 
-// 1. COOPERATIVE ROUND-ROBIN TASK SCHEDULER & CONTEXT MANAGER
 struct task_control_block { int id; unsigned int esp; unsigned int eip; int state; };
 struct task_control_block task_queue[4];
 int current_task_index = 0;
@@ -23,61 +22,37 @@ void create_task(void (*entry_point)(), void* stack_space) {
 void task_switch() {
     if (total_tasks <= 1) return;
     task_queue[current_task_index].state = 0;
-    current_task_index = (current_task_index + 1) % total_tasks;
-    task_queue[current_task_index].state = 1;
-    __asm__ __volatile__ ("movl %0, %%esp \n\t jmp *%1" : : "r"(task_queue[current_task_index].esp), "r"(task_queue[current_task_index].eip) : "memory");
-}
-
-void pit_interrupt_scheduler_hook() {
-    if (total_tasks <= 1) { outb(0x20, 0x20); return; }
-    task_queue[current_task_index].state = 0;
-    current_task_index = (current_task_index + 1) % total_tasks;
-    task_queue[current_task_index].state = 1;
-    outb(0x20, 0x20);
-    __asm__ __volatile__ ("movl %0, %%esp \n\t jmp *%1" : : "r"(task_queue[current_task_index].esp), "r"(task_queue[current_task_index].eip) : "memory");
-}
-
+    current_task_index = (current_task_index + 1) 
 void print_process_status() {
     extern void print(const char* str);
     extern void print_char(char c);
-    print("PID  |  Status   |  Memory Address\n---------------------------------\n");
+    print("PID  |  Status   |  Memory Addressn---------------------------------n");
     for (int i = 0; i < total_tasks; i++) {
         print(" "); print_char('0' + task_queue[i].id);
         print(task_queue[i].state == 1 ? "   |  RUNNING  |  0x" : "   |  READY    |  0x");
         unsigned int addr = task_queue[i].eip;
         char buf[16]; int idx = 0;
-        if (addr == 0) { print("00000000\n"); continue; }
-        while (addr > 0) {
-            unsigned int rem = addr % 16;
-            buf[idx++] = (rem < 10) ? '0' + rem : 'A' + (rem - 10);
-            addr /= 16;
-        }
+        if (addr == 0) { print("00000000n"); continue; }
+        while (addr > 0) { unsigned int rem = addr % 16; buf[idx++] = (rem < 10) ? '0' + rem : 'A' + (rem - 10); addr /= 16; }
         while (idx > 0) print_char(buf[--idx]);
-        print("\n");
+        print("n");
     }
 }
 
-// 2. KERNEL-SPACE INTER-PROCESS COMMUNICATION MAILBOX
 char ipc_mailbox[128];
 int ipc_has_message = 0;
 
 void ipc_send(const char* message) {
-    int i = 0;
-    while (message[i] != '\0' && i < 127) { ipc_mailbox[i] = message[i]; i++; }
-    ipc_mailbox[i] = '\0'; ipc_has_message = 1;
+    int i = 0; while (message[i] != '0' && i < 127) { ipc_mailbox[i] = message[i]; i++; }
+    ipc_mailbox[i] = '0'; ipc_has_message = 1;
 }
 
 void ipc_receive() {
     extern void print(const char* str);
-    if (ipc_has_message == 1) {
-        print("Mailbox Message: "); print(ipc_mailbox); print("\n");
-        ipc_has_message = 0;
-    } else {
-        print("Mailbox Empty.\n");
-    }
+    if (ipc_has_message == 1) { print("Mailbox Message: "); print(ipc_mailbox); print("n"); ipc_has_message = 0; }
+    else { print("Mailbox Empty.n"); }
 }
 
-// 3. BACKGROUND SPINNING ASSET WORKER THREAD
 void background_worker_task() {
     volatile unsigned short* vram = (volatile unsigned short*)0xB8000;
     unsigned char spin[4] = {'|', '/', '-', ''};
@@ -89,88 +64,50 @@ void background_worker_task() {
     }
 }
 
-// 4. MULTI-USER PROFILE RECORDS DATABASE
-struct user_profile { char username[16]; unsigned int password_hash; int active; };
-struct user_profile system_users[5];
-int total_system_users = 0;
-
+unsigned int target_password_hash = 208947265;
 unsigned int app_calculate_string_hash(const char* str) {
     unsigned int hash = 5381; int i = 0;
-    while (str[i] != '\0') { hash = ((hash << 5) + hash) + str[i]; i++; }
+    while (str[i] != '0') { hash = ((hash << 5) + hash) + str[i]; i++; }
     return hash;
 }
 
-void app_init_users() {
-    system_users[0].active = 1; system_users[0].password_hash = 208947265; // "ziggy" prime code hash
-    unsigned char* adm = (unsigned char*)"admin";
-    for(int i=0; adm[i]!='\0'; i++) system_users[0].username[i] = adm[i];
-    total_system_users = 1;
+int app_verify_system_login(const char* input_password) {
+    extern void print(const char* str);
+    unsigned int input_hash = app_calculate_string_hash(input_password);
+    if (input_hash == target_password_hash) { print("nAuthentication Successful!n"); return 1; }
+    print("nAccess Denied!n"); return 0;
 }
 
-void app_user_add(const char* name, const char* raw_pass) {
-    extern void print(const char* str);
-    if (total_system_users >= 5) { print("Error: Max users reached!\n"); return; }
-    int slot = total_system_users;
-    system_usecat << 'EOF' >> src/app.c
-void app_crypt_file() {
-struct file_struct { char name[32]; char content[128]; int size; int active; };
-extern struct file_struct ramfs[10];
-if(ramfs[0].active == 1) {
-char key = (char)(target_password_hash & 0xFF);
-for(int i = 0; ramfs[0].content[i] != '\0' && i < 128; i++) ramfs[0].content[i] ^= key;
-print("\nCipher process complete.\n");
+unsigned int rand_seed = 12345;
+unsigned int quick_rand() { rand_seed = (rand_seed * 1103515245 + 12345) \& 0x7FFFFFFF; return rand_seed; }
+
+void app_matrix_screensaver() {
+    extern void print(const char* str); extern void shell_clear(); extern int keyboard_has_key();
+    volatile unsigned short* vram = (volatile unsigned short*)0xB8000;
+    for(int i = 0; i < 2000; i++) vram[i] = (0x0F << 8) | ' ';
+    print("Press any key to exit Matrix mode...n");
+    while(!keyboard_has_key()) {
+        int x = quick_rand() % 80; int y = quick_rand() % 25; char c = 33 + (quick_rand() % 93);
+        vram[y * 80 + x] = (0x0A << 8) | c; for(volatile int d = 0; d < 10000; d++);
+    }
+    shell_clear();
 }
-}
+
 void app_run_calculator(const char* expr) {
-extern void print_char(char c);
-int num1 = 0, num2 = 0, idx = 0; char op = 0;
-while(expr[idx] == ' ' || (expr[idx] >= 'a' && expr[idx] <= 'z')) idx++;
-while(expr[idx] >= '0' && expr[idx] <= '9') { num1 = num1 * 10 + (expr[idx] - '0'); idx++; }
-while(expr[idx] == ' ') idx++;
-if(expr[idx] == '+' || expr[idx] == '-' || expr[idx] == '*' || expr[idx] == '/') { op = expr[idx]; idx++; }
-while(expr[idx] == ' ') idx++;
-while(expr[idx] >= '0' && expr[idx] <= '9') { num2 = num2 * 10 + (expr[idx] - '0'); idx++; }
-print("\nResult: "); int res = 0;
-if(op == '+') res = num1 + num2;
-else if(op == '-') res = num1 - num2;
-else if(op == '*') res = num1 * num2;
-else if(op == '/') { if(num2 == 0) { print("Error: Div by 0!\n"); return; } res = num1 / num2; }
-char buf[16]; int i = 0;
-if(res == 0) { print_char('0'); print("\n"); return; }
-while(res > 0) { buf[i++] = '0' + (res % 10); res /= 10; }
-while(i > 0) print_char(buf[--i]);
-print("\n");
+    extern void print(const char* str); extern void print_char(char c);
+    int num1 = 0, num2 = 0, idx = 0; char op = 0;
+    while(expr[idx] == ' ' || (expr[idx] >= 'a' && expr[idx] <= 'z')) idx++;
+    while(expr[idx] >= '0' && expr[idx] <= '9') { num1 = num1 * 10 + (expr[idx] - '0'); idx++; }
+    while(expr[idx] == ' ') idx++;
+    if(expr[idx] == '+' || expr[idx] == '-' || expr[idx] == '*' || expr[idx] == '/') { op = expr[idx]; idx++; }
+    while(expr[idx] == ' ') idx++;
+    while(expr[idx] >= '0' && expr[idx] <= '9') { num2 = num2 * 10 + (expr[idx] - '0'); idx++; }
+    print("nResult: "); int res = 0;
+    if(op == '+') res = num1 + num2; else if(op == '-') res = num1 - num2; else if(op == '*') res = num1 * num2;
+    else if(op == '/') { if(num2 == 0) { print("Error: Div by 0!n"); return; } res = num1 / num2; }
+    char buf[16]; int i = 0; if(res == 0) { print_char('0'); print("n"); return; }
+    while(res > 0) { buf[i++] = '0' + (res % 10); res /= 10; }
+    while(i > 0) print_char(buf[--i]);
+    print("n");
 }
-void app_play_snake() {
-extern unsigned char keyboard_read(); extern char scancode_to_ascii(unsigned char scancode);
-extern void play_sound(unsigned int freq); extern void nosound(); extern void shell_clear(); extern int keyboard_has_key();
-volatile unsigned short* vram = (volatile unsigned short*)0xB8000;
-shell_clear();
-int snake_x[100]; int snake_y[100]; int snake_length = 3; int dir_x = 1; int dir_y = 0;
-for(int i = 0; i < snake_length; i++) { snake_x[i] = 10 - i; snake_y[i] = 10; }
-int food_x = 20; int food_y = 15; int game_over = 0;
-print("SNAKE GAME - WASD keys to move. Q to quit.\n");
-while(!game_over) {
-for(int i = 80; i < 2000; i++) vram[i] = (0x0F << 8) | ' ';
-vram[food_y * 80 + food_x] = (0x0B << 8) | 'O';
-if(keyboard_has_key()) {
-unsigned char code = keyboard_read(); char key = scancode_to_ascii(code);
-if(key == 'w' && dir_y == 0) { dir_x = 0; dir_y = -1; }
-else if(key == 's' && dir_y == 0) { dir_x = 0; dir_y = 1; }
-else if(key == 'a' && dir_x == 0) { dir_x = -1; dir_y = 0; }
-else if(key == 'd' && dir_x == 0) { dir_x = 1; dir_y = 0; }
-else if(key == 'q') break;
-}
-for(int i = snake_length - 1; i > 0; i--) { snake_x[i] = snake_x[i-1]; snake_y[i] = snake_y[i-1]; }
-snake_x[0] += dir_x; snake_y[0] += dir_y;
-if(snake_x[0] < 0 || snake_x[0] > 79 || snake_y[0] < 1 || snake_y[0] > 24) game_over = 1;
-if(snake_x[0] == food_x && snake_y[0] == food_y) {
-if(snake_length < 100) snake_length++;
-food_x = 2 + (quick_rand() % 75); food_y = 2 + (quick_rand() % 20);
-play_sound(880); for(volatile int d=0; d<100000; d++); nosound();
-}
-for(int i = 0; i < snake_length; i++) vram[snake_y[i] * 80 + snake_x[i]] = (0x0A << 8) | 'S';
-for(volatile int d = 0; d < 3000000; d++) ;
-}
-shell_clear(); print("Game Over!\n-> ");
-}
+// Build Trigger Stamp: Thu Jun 11 04:57:11 UTC 2026
