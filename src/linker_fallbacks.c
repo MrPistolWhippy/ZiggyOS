d0d305990ea941d226cc3499d04ad5581ec4ef86
MrPistolWhippy <mrpistolwhippy@://github.com>
2026-06-11 15:09:40 +0000

    Restore local vector handlers and print routine to satisfy linker

--- src/linker_fallbacks.c
+++ src/linker_fallbacks.c
@@ -1,1 +1,24 @@
-// Intentionally left blank to fully yield to the cloud injection steps
+#include <stdint.h>
+
+// Core System Vector Exception Handlers
+void Reset_Handler(void) {
+    extern void kernel_main(void);
+    kernel_main();
+}
+
+void Default_Handler(void) {
+    while (1);
+}
+
+void HardFault_Handler(void) __attribute__((weak, alias("Default_Handler")));
+void PendSV_Handler(void)    __attribute__((weak, alias("Default_Handler")));
+void SysTick_Handler(void)   __attribute__((weak, alias("Default_Handler")));
+void UART0_Handler(void)     __attribute__((weak, alias("Default_Handler")));
+
+// System-wide print utility string loop
+void print(const char *str) {
+    extern void uart_putc(char c);
+    while (*str) {
+        uart_putc(*str++);
+    }
+}
