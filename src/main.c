8c8e5ee494137cca0822f6aeeef6f2ddca397afc
MrPistolWhippy <mrpistolwhippy@://github.com>
2026-06-11 18:04:15 +0000

    Fix function signature mismatch for jump_to_user_mode

--- src/main.c
+++ src/main.c
@@ -84,4 +84,4 @@ void watchdog_kick(void) {}
 void init_syscall_vector_gate(void) {}
 void init_fat_filesystem(void) {}
 void init_ring1_device_drivers(void) {}
-void jump_to_user_mode(void) {}
+void jump_to_user_mode(void (user_func)(void)) { (void)user_func; }
