5a1861162896ca3b5b7d603d6174be753cb47a90
MrPistolWhippy <mrpistolwhippy@://github.com>
2026-06-11 02:34:44 +0000

    Feature: Implement command string buffer with hardware reboot hook

--- src/main.c
+++ src/main.c
@@ -6,6 +6,21 @@ void print(const char* str);
 void print_char(char c);
 void shell_backspace();
 
+char cmd_buffer[256];
+int cmd_index = 0;
+
+void process_command() {
+    print("\n");
+    if (cmd_buffer[0] == 'r' && cmd_buffer[1] == 'e' && cmd_buffer[2] == 'b' && cmd_buffer[3] == 'o' && cmd_buffer[4] == 'o' && cmd_buffer[5] == 't') {
+        print("Rebooting system...\n");
+        outb(0x64, 0xFE); 
+    } else {
+        print("Unknown command: ");
+        print(cmd_buffer);
+        print("\n-> ");
+    }
+}
+
 void kernel_main() {
     print("ZiggyOS Active CLI Shell\n-> ");
 
@@ -15,13 +30,21 @@ void kernel_main() {
         
         if (ascii != 0) {
             if (ascii == '\n') {
-                print("\n-> ");
+                cmd_buffer[cmd_index] = '\0'; 
+                process_command();
+                cmd_index = 0; 
             } 
             else if (ascii == '\b') {
-                shell_backspace(); // Process destructive backspace on screen
+                if (cmd_index > 0) {
+                    cmd_index--;
+                    shell_backspace();
+                }
             } 
             else {
-                print_char(ascii); // Print normal text letters
+                if (cmd_index < 255) {
+                    cmd_buffer[cmd_index++] = ascii;
+                    print_char(ascii);
+                }
             }
         }
     }
